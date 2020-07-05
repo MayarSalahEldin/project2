a9a41bbb506542d2f39264dd9f645608c33f0eae
Mayar Salah ELDin <mayar_salah13@yahoo.com>
2020-07-05 19:06:43 +0200

    yarab

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+
+
+  scope :api, defaults: {format: :json} do
+  resources :foos
+  resources :bars
+  resources :states
+  resources :cities
+  end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/ui" => "ui#index"
+  get "/ui#" => "ui#index"
+  root "ui#index"
+end
