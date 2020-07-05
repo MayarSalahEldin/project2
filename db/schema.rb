96835e1737cfb50988b9fe8ed9f48db490964cb6
Mayar Salah ELDin <mayar_salah13@yahoo.com>
2020-07-05 20:23:44 +0200

    mayar

--- db/schema.rb
+++ db/schema.rb
@@ -15,12 +15,16 @@ ActiveRecord::Schema.define(version: 20180806035352) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "cities", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+  #create_table "cities", force: :cascade do |t|
+  #  t.string "name"
+  #  t.datetime "created_at", null: false
+  #  t.datetime "updated_at", null: false
+  #end
 
- 
+  #create_table "foos", force: :cascade do |t|
+   # t.string "name"
+   # t.datetime "created_at", null: false
+   # t.datetime "updated_at", null: false
+  #end
 
 end
