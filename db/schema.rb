4d38bf2982156fc5f2136be36a89642994149a98
Mayar Salah ELDin <mayar_salah13@yahoo.com>
2020-07-05 20:20:45 +0200

    mayar

--- db/schema.rb
+++ db/schema.rb
@@ -21,10 +21,6 @@ ActiveRecord::Schema.define(version: 20180806035352) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "foos", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
+ 
 
 end
